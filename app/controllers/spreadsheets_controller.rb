4a507b71a71600cd4ca5958ed77fa09744f8127c
Benjamin <benj@zagorski.com>
2017-06-06 21:13:06 -0700

    GET, POST, and DELETE are all working locally from Postman

--- app/controllers/spreadsheets_controller.rb
+++ app/controllers/spreadsheets_controller.rb
@@ -2,7 +2,31 @@ class SpreadsheetsController < ApplicationController
   skip_before_action :verify_authenticity_token
   def new
   end
+
   def create
-    render json: {Post: "Success!"}
+    # render json: params
+    puts params.inspect
+    @spreadsheet = Spreadsheet.new(spreadsheet_params)
+    if @spreadsheet.save
+      render json: @spreadsheet
+    else
+      render json: {Post: "failure"}
+    end
+  end
+
+  def index
+    render json: Spreadsheet.all
+  end
+
+  def destroy
+    Spreadsheet.all.each do |request|
+      request.destroy
+    end
+    render json: {allSpreadsheets: "deleted"}
+  end
+
+  private
+  def spreadsheet_params
+    params.require(:spreadsheet).permit(:instructions)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'spreadsheets/new'
-  resources :spreadsheets
+  # resources :spreadsheets
   root 'spreadsheets#new'
+  get "/spreadsheets" => "spreadsheets#index"
+  post "/spreadsheets" => "spreadsheets#create"
+  delete "/spreadsheets" => "spreadsheets#destroy"
 end
