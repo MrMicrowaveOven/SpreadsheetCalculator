d9ddd0406816e87b7545be80b426a89d3defe13e
Benjamin <benj@zagorski.com>
2017-06-08 08:59:52 -0700

    Added check_table_count with tests to Spreadsheet model

--- app/models/spreadsheet.rb
+++ app/models/spreadsheet.rb
@@ -1,4 +1,17 @@
 class Spreadsheet < ApplicationRecord
+    # VALID_INSTRUCTIONS_REGEX =
     validates :instructions, presence: true,
     format: { with: /\A([0-9]+\s[0-9]+)$(((\n^(([A-Z]+)([0-9]+)|[0-9]+)((\s(([A-Z]+)([0-9]+)|[0-9]+|[-+*\/]+))*))*))\Z/, message: "improper format" }
+    # validate do
+    #   check_table_count
+    # end
+
+    def check_table_count
+      # return false if !self.valid?
+      spreadsheet_layout = self.instructions.split("\n").first
+      spreadsheet_mults = spreadsheet_layout.split
+      spreadsheet_size = spreadsheet_mults[0].to_i * spreadsheet_mults[1].to_i
+      raise "incorrect spreadsheet size" if self.instructions.split("\n").length - 1 != spreadsheet_size
+      true
+    end
 end
--- test/models/spreadsheet_test.rb
+++ test/models/spreadsheet_test.rb
@@ -1,12 +1,17 @@
 require 'test_helper'
 
 class SpreadsheetTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-  end
   test "should not save an empty spreadsheet" do
     spreadsheet = Spreadsheet.new
     assert_not spreadsheet.save
+  end
+  test "should not allow initialization of a spreadsheet with an incorrect key" do
+    exception = assert_raises(Exception) {
+      spreadsheet = Spreadsheet.new({
+        instluctions: "3 2\nB2\n4 3 *\nC2\nA1 B1 / 2 +\n13\nB1 A2 / 2 *"
+      })
+    }
+    assert_equal("unknown attribute 'instluctions' for Spreadsheet.", exception.message)
   end
   test "should not save a spreadsheet that doesn't begin with a size" do
     spreadsheet = Spreadsheet.new({instructions: "Hello"})
@@ -20,16 +25,24 @@ class SpreadsheetTest < ActiveSupport::TestCase
     spreadsheet = Spreadsheet.new({instructions: "3 2\nB2\n4 3 *\nC2\nA1 B1 / 2 +\n13\nB1 A2 / 2 *"})
     assert spreadsheet.save
   end
-  test "should reject a spreadsheet with an unknown character" do
+  test "should not save a spreadsheet with an unknown character" do
     spreadsheet = Spreadsheet.new({instructions: "3 2\nB2\n4 3 %\nC2\nA1 B1 / 2 +\n13\nB1 A2 / 2 *"})
     assert_not spreadsheet.save
   end
-  test "should reject a spreadsheet with too many linebreaks" do
-    spreadsheet = Spreadsheet.new({instructions: "3 2\n\nB2\n4 3 %\nC2\nA1 B1 / 2 +\n13\nB1 A2 / 2 *"})
+  test "should not save a spreadsheet with too many linebreaks" do
+    spreadsheet = Spreadsheet.new({instructions: "3 2\n\nB2\n4 3 *\nC2\nA1 B1 / 2 +\n13\nB1 A2 / 2 *"})
     assert_not spreadsheet.save
   end
-  test "should reject a spreadsheet with too many spaces" do
-    spreadsheet = Spreadsheet.new({instructions: "3 2\nB2  \n4 3 %\nC2\nA1 B1 / 2 +\n13\nB1 A2 / 2 *"})
+  test "should not save a spreadsheet with too many spaces" do
+    spreadsheet = Spreadsheet.new({instructions: "3 2\nB2  \n4 3 *\nC2\nA1 B1 / 2 +\n13\nB1 A2 / 2 *"})
     assert_not spreadsheet.save
+  end
+  
+  class TableCountCheckTest < ActiveSupport::TestCase
+    test "should reject a spreadsheet with an incorrect table size" do
+      spreadsheet = Spreadsheet.new({instructions: "2 2\nB2\n5\n4 3 *\nC2\nA1 B1 / 2 +\n13\nB1 A2 / 2 *"})
+      exception = assert_raises(Exception) {spreadsheet.check_table_count}
+      assert_equal("incorrect spreadsheet size", exception.message)
+    end
   end
 end
