a56d2adf949255ff38b307de0f3d16f3fad43525
Benjamin <benj@zagorski.com>
2017-06-07 22:27:23 -0700

    Added Spreadsheet model with regex validation and basic testing

--- app/models/spreadsheet.rb
+++ app/models/spreadsheet.rb
@@ -1,2 +1,4 @@
 class Spreadsheet < ApplicationRecord
+    validates :instructions, presence: true,
+    format: { with: /\A([0-9]+\s[0-9]+)$(((\n^(([A-Z]+)([0-9]+)|[0-9]+)((\s(([A-Z]+)([0-9]+)|[0-9]+|[-+*\/]+))*))*))\Z/, message: "improper format" }
 end
--- test/models/spreadsheet_test.rb
+++ test/models/spreadsheet_test.rb
@@ -1,7 +1,31 @@
 require 'test_helper'
 
 class SpreadsheetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "the truth" do
+    assert true
+  end
+  test "should not save an empty spreadsheet" do
+    spreadsheet = Spreadsheet.new
+    assert_not spreadsheet.save
+  end
+  test "should not save a spreadsheet that doesn't begin with a size" do
+    spreadsheet = Spreadsheet.new({instructions: "Hello"})
+    assert_not spreadsheet.save
+  end
+  test "should save a spreadsheet with a blank size" do
+    spreadsheet = Spreadsheet.new({instructions: "0 0"})
+    assert spreadsheet.save
+  end
+  test "should save a standard spreadsheet" do
+    spreadsheet = Spreadsheet.new({instructions: "3 2\nB2\n4 3 *\nC2\nA1 B1 / 2 +\n13\nB1 A2 / 2 *"})
+    assert spreadsheet.save
+  end
+  test "should reject a spreadsheet with an unknown character" do
+    spreadsheet = Spreadsheet.new({instructions: "3 2\nB2\n4 3 %\nC2\nA1 B1 / 2 +\n13\nB1 A2 / 2 *"})
+    assert_not spreadsheet.save
+  end
+  test "should reject a spreadsheet with too many spaces" do
+    spreadsheet = Spreadsheet.new({instructions: "3 2\n\nB2\n4 3 %\nC2\nA1 B1 / 2 +\n13\nB1 A2 / 2 *"})
+    assert_not spreadsheet.save
+  end
 end
