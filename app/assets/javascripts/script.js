36ceeead914ea9f78c874e78502406f81a62e664
Benjamin <benj@zagorski.com>
2017-06-08 17:39:33 -0700

    Frontend now makes API calls, and displays results in the table.  No 400 handling yet

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,3 +1,7 @@
+var ALPHABET = ["A","B", "C", "D", "E", "F", "G", "H",
+  "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+  "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
 function evaluateSpreadsheet() {
   var spreadsheetInstructions = getInput();
   makeRequest(spreadsheetInstructions);
@@ -14,7 +18,8 @@ function makeRequest(spreadsheetInstructions) {
     data: {spreadsheet: {instructions: spreadsheetInstructions}},
     dataType: "json",
     success: function (res) {
-        console.log(res);
+      // console.log(res);
+      displayResults(res);
     },
     error: function (xhr, status, error) {
       console.log(xhr.responseText);
@@ -23,4 +28,35 @@ function makeRequest(spreadsheetInstructions) {
       // console.log(error);
     }
   });
+}
+
+function displayResults(res) {
+  var table = $("#spreadsheet_results");
+  var tableLayout = res.instructions.split("\n");
+  var tableDimensions = tableLayout.shift().split(" ");
+  var numColumns = tableDimensions[0];
+  var numRows = tableDimensions[1];
+
+  $("#columnHeader").empty();
+  $("#rows").empty();
+
+  $("#columnHeader").append("<th>#</th>");
+
+  for (var columnIndex = 0; columnIndex < numColumns; columnIndex++) {
+    $("#columnHeader").append("<th>" + ALPHABET[columnIndex] + "</th>");
+  }
+  var currentRow;
+  var cellValue;
+  for (var rowIndex = 1; rowIndex <= numRows; rowIndex++) {
+    $("#rows").append(
+      '<tr id="row' + rowIndex
+      + '"><th scope="row">' + rowIndex
+      + '</th></tr>'
+    );
+    currentRow = $("#row" + rowIndex);
+    for (var columnIndex = 0; columnIndex < numColumns; columnIndex++) {
+      cellValue = tableLayout.shift();
+      currentRow.append("<td>" + cellValue + "</td>");
+    }
+  }
 }
